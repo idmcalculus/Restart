5c691b2db3658518711b45df0922d9b16ede49db
Damilola Michael Ige <idm.calculus@gmail.com>
2022-01-25 07:50:48 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by IGE DAMILOLA MICHAEL on 25/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
